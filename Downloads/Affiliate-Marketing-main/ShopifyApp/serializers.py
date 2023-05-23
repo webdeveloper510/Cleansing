d1af20e0def5823877fd8a4953d159399db90fb6
webdeveloper510 <developerweb6@gmail.com>
2023-05-23 11:12:09 +0530

    Delete Downloads/Affiliate-Marketing-main directory

--- Downloads/Affiliate-Marketing-main/ShopifyApp/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from ShopifyApp.models import *
-
-
-
-class ShopifySerializer(serializers.ModelSerializer):
-    class Meta:
-        model=Coupon
-        fields="__all__"
