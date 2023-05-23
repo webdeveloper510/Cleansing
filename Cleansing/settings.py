384f81c3b2c4fdb0f0bec17c5fe2b8ee87ac2344
webdeveloper510 <developerweb6@gmail.com>
2023-05-23 18:37:19 +0530

    changes

--- Cleansing/settings.py
+++ Cleansing/settings.py
@@ -77,23 +77,23 @@ WSGI_APPLICATION = 'Cleansing.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-#print
 # DATABASES = {
 #     'default': {
-#     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#     'NAME': 'test', 
-#     'USER': 'postgres',
-#     'PASSWORD': '1234',
-#     'HOST': '127.0.0.1', 
-#     'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+#print
+DATABASES = {
+    'default': {
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': 'test', 
+    'USER': 'postgres',
+    'PASSWORD': '1234',
+    'HOST': '23.95.94.206', 
+    'PORT': '5432',
+    }
+}
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
